d3a0b7d1cbee807d38373df85f0c5b7c9aab3d1d
keshakostya <kkkonstantin.kozlov@yandex.ru>
2024-11-15 18:41:55 +0500

    add start and stop functions

--- net-filter-ctl/net_filter_ctl_start.c
+++ net-filter-ctl/net_filter_ctl_start.c
@@ -1,6 +1,38 @@
+#include <stdio.h>
+#include <net/if.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <xdp/libxdp.h>
 #include "net_filter_ctl.h"
 
 int net_filter_ctl_start(struct net_filter_options *opts)
 {
+  int ret, ifindex, map_fd;
+  struct xdp_program *prog;
+  struct bpf_object *bpf_obj;
+
+  ifindex = if_nametoindex(opts->ifname);
+  if (ifindex == 0)
+    return -1;
+
+  prog = xdp_program__open_file(NET_FILTER_PROGRAM_FILE, 
+                                NET_FILTER_PROGRAM_SECTION, NULL);
+  if (!prog)
+  {
+    return -1;
+  }
+
+  ret = xdp_program__attach(prog, ifindex, XDP_MODE_SKB, 0);
+  if (ret)
+  {
+    xdp_program__close(prog);
+    return ret;
+  }
+
+  bpf_obj = xdp_program__bpf_obj(prog);
+  ret = bpf_object__pin_maps(bpf_obj, "/sys/fs/bpf/ens160");
+	if (ret)
+		return -1;
+
   return 0;
 }
--- net-filter-ctl/net_filter_ctl_stop.c
+++ net-filter-ctl/net_filter_ctl_stop.c
@@ -1,6 +1,78 @@
+#include <stdio.h>
+#include <net/if.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <xdp/libxdp.h>
+#include <string.h>
+#include <unistd.h>
 #include "net_filter_ctl.h"
 
 int net_filter_ctl_stop(struct net_filter_options *opts)
 {
-  return 0;
+  int ifindex = 0;
+  struct xdp_multiprog *mp;
+  struct xdp_program *prog = NULL;
+  int mode = 0;
+  int ret;
+  struct bpf_object *bpf_obj;
+
+  ifindex = if_nametoindex(opts->ifname);
+  if (!ifindex)
+  {
+    return -1;
+  }
+
+  prog = xdp_program__open_file(NET_FILTER_PROGRAM_FILE, 
+                                NET_FILTER_PROGRAM_SECTION, NULL);
+  if (!prog)
+  {
+    return -1;
+  }
+  
+  bpf_obj = xdp_program__bpf_obj(prog);
+  ret = bpf_object__unpin_maps(bpf_obj, "/sys/fs/bpf/ens160");
+
+  mp = xdp_multiprog__get_from_ifindex(ifindex);
+  if (!mp)
+  {
+    ret = -1;
+    goto out;
+  }
+  else if (libxdp_get_error(mp))
+  {
+    ret = -1;
+    goto out;
+  }
+
+  prog = NULL;
+  while ((prog = xdp_multiprog__next_prog(prog, mp)))
+  {
+    if (strcmp(xdp_program__name(prog), NET_FILTER_PROGRAM_NAME) == 0)
+    {
+      mode = xdp_multiprog__attach_mode(mp);
+      goto found;
+    }
+  }
+
+  if (xdp_multiprog__is_legacy(mp))
+  {
+    prog = xdp_multiprog__main_prog(mp);
+    if (strcmp(xdp_program__name(prog), NET_FILTER_PROGRAM_NAME) == 0)
+    {
+      mode = xdp_multiprog__attach_mode(mp);
+      goto found;
+    }
+  }
+
+  prog = xdp_multiprog__hw_prog(mp);
+	if (strcmp(xdp_program__name(prog), NET_FILTER_PROGRAM_NAME) == 0)
+  {
+		mode = XDP_MODE_HW;
+		goto found;
+	}
+found:
+  xdp_program__detach(prog, ifindex, mode, 0);
+out:
+  xdp_multiprog__close(mp);
+  return ret;
 }
